f6e93f826424026425273560340d4edd7b06c5bb
Lu <luacosta.fc@gmail.com>
2018-11-23 00:19:07 -0500

    Fix Syntax Error

--- db/migrate/20181121213430_add_done_to_todo.rb
+++ db/migrate/20181121213430_add_done_to_todo.rb
@@ -1,5 +1,5 @@
 class AddDoneToTodo < ActiveRecord::Migration[5.2]
   def change
-    add_column :todos, :done, :bolean, default: false
+    add_column :todos, :done, :boolean, default: false
   end
 end
